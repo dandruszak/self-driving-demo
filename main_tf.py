8ffdcaa87b0f7f484e5d2a20d6334562cda0dee3
Dominik Andruszak <39803943+dandruszak@users.noreply.github.com>
2018-06-14 13:29:55 +0200

    Update main_tf.py

--- main_tf.py
+++ main_tf.py
@@ -119,8 +119,8 @@ def main():
     # Training flags - feel free to play with that!
     flags.DEFINE_integer("batch", 64, "Batch size")
     flags.DEFINE_integer("time", 1, "Number of frames per sample")
-    flags.DEFINE_integer("steps_per_epoch", 10000, "Number of training steps per epoch")
-    flags.DEFINE_integer("nb_epochs", 200, "Number of epochs")
+    flags.DEFINE_integer("steps_per_epoch", 1000, "Number of training steps per epoch")
+    flags.DEFINE_integer("nb_epochs", 1, "Number of epochs")
 
 
     # Model flags - feel free to play with that!
